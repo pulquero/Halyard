d21558db44e06440fad959d8d1db3e58bc3aa8fb
Mark Hale <mark.hale@physics.org>
2024-01-04 14:26:15 +0000

    Fix HRDF mime type.

--- rio/src/main/java/com/msd/gin/halyard/rio/HRDF.java
+++ rio/src/main/java/com/msd/gin/halyard/rio/HRDF.java
@@ -3,7 +3,7 @@ package com.msd.gin.halyard.rio;
 import org.eclipse.rdf4j.rio.RDFFormat;
 
 public final class HRDF {
-	public static final RDFFormat FORMAT = new RDFFormat("HRDF", "hrdf", null, "hrdf", RDFFormat.NO_NAMESPACES, RDFFormat.SUPPORTS_CONTEXTS, RDFFormat.SUPPORTS_RDF_STAR);
+	public static final RDFFormat FORMAT = new RDFFormat("HRDF", "application/x-hrdf", null, "hrdf", RDFFormat.NO_NAMESPACES, RDFFormat.SUPPORTS_CONTEXTS, RDFFormat.SUPPORTS_RDF_STAR);
 	static final int TRIPLES = 0;
 	static final int QUADS = 4;
 	static final int O = 1;
